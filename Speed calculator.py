06e4e0557423f27774a9fba9d9e474a956f1a9e3
Oblong101 <121264821+Oblong101@users.noreply.github.com>
2022-12-29 01:15:49 +0000

    Add files via upload

---
+++ Speed calculator.py
@@ -0,0 +1,7 @@
+#This is a small progrm used to calculate the speed of an object
+
+distance = float(input("How far did the object travel in metres(m)? "))
+time = float(input("How long was the object traveling for in seconds(s)? "))
+
+speed = distance/time
+print("The speed of the object in metres per second(m/s) is:", speed, "m/s")
